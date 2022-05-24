2c4150d32d85a0c69666fc24ba956da86d5f26b4
silikhe <silaswans@gmail.com>
2022-05-24 23:27:16 +0300

    update component

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>Silikhe</code> 
         </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
+          rel=""
         >
-          Learn React
+          Silas Silikhe
         </a>
       </header>
     </div>
